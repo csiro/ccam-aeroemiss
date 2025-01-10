bfdf3ec2a900970c91794758124e9f652bf4fc3c
Thatcher, Marcus (O&A Aspendale) <Marcus.Thatcher@csiro.au>
2025-01-10 15:38:58 +1100

    Additional improvement for aeroemiss dms and dust emissions near poles

--- version.h
+++ version.h
@@ -1,2 +1,2 @@
 character(len=*), parameter :: version = &
-'AEROEMISS Date:   Fri Oct 25 10:25:17 2024 +1100 commit 47e591160194f298091e84182db3e93b4dd0a0af'
+'AEROEMISS Date:   Fri Jan 10 14:08:59 2025 +1100 commit 34d370777ea69138ca4c5ab3c61b5913e2f08f0e'
